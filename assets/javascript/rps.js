ec0b9f4db07f82fb64e616f532ba76a02d10ef3f
Nic_Logan <nicholaslogan255@gmail.com>
2018-06-28 19:41:42 -0500

    referenced in-class examples

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,3 +1,5 @@
+
+// 1. Initialize Firebase
 var config = {
     apiKey: "AIzaSyCHNBdbp9D_Ut7fJB3jlxBBV1iVpuqTR94",
     authDomain: "myfirstfirebase-57571.firebaseapp.com",
@@ -5,122 +7,110 @@ var config = {
     projectId: "myfirstfirebase-57571",
     storageBucket: "myfirstfirebase-57571.appspot.com",
     messagingSenderId: "326583901736"
-
-};
-
-firebase.initializeApp(config);
-
-// Create a variable to reference the database.
-var database = firebase.database();
-
-// -------------------- Multi-user connection code --------------------------------//
-
-var player1 = null;
-var player2 = null;
-
-// connectionsRef references a specific location in our database.
-// All of our connections will be stored in this directory.
-var connectionsRef = database.ref("/connections");
-
-// '.info/connected' is a special location provided by Firebase that is updated every time
-// the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-var connectedRef = database.ref(".info/connected");
-
-// When the client's connection state changes...
-connectedRef.on("value", function (snap) {
-
-    // If they are connected..
-    if (snap.val()) {
-
-        // Add user to the connections list.
-        var con = connectionsRef.push(true);
-
-        // Remove user from the connection list when they disconnect.
-        con.onDisconnect().remove();
-    }
-});
-
-// When first loaded or when the connections list changes...
-connectionsRef.on("value", function (snap) {
-
-    // Display the viewer count in the html.
-    // The number of online users is the number of children in the connections list.
-    $("#watchers").text(snap.numChildren());
-});
-
-// -----------------------   End connetion section ----------------------------
-
-
-// At the page load and subsequent value changes, get a snapshot of the local data.
-// This callback allows the page to stay updated with the values in firebase node "clicks"
-database.ref("trains").on("value", function (snapshot) {
-
-    // Print the local data to the console.
-    console.log(snapshot.val());
-
-
-    // todo: load in train schedule
-
-
-    // If any errors are experienced, log them to console.
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-});
-
-
-
-//  On Click event associated with the add-train function
-$(document).on("click", "#add-train", function (event) {
-
-    // prevent form from submitting
+  };
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  // 2. Button for adding trains
+  $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
+  
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainFreq = $("#Frequency-input").val().trim();
+    var trainStart = moment($("#start-time-input").val().trim(), "MM/DD/YYYY").format("X");
 
-    console.log("Submitting answers");
+    //empName empRole empRate empStart
+  
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+      name: trainName,
+      destination: trainDest,
+      frequency: trainFreq,
+      start: trainStart
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log("input: "+newTrain.name);
+    console.log("input: "+newTrain.destination);
+    console.log("input: "+newTrain.frequency);
+    console.log("input: "+newTrain.start);
+  
+    //alert("train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#Frequency-input").val("");
+    $("#start-time-input").val("");
+  });
+  
+  // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainFreq = childSnapshot.val().frequency;
+    var trainStart = childSnapshot.val().start;
 
-    // todo: change to default to false after input validation coded
-    dataValidated = true;
+    // Prettify the train times
+    trainFreqTime = moment(trainFreq, "mm");
+    trainStartTime = moment(trainStart, "HH:mm");
+  
+    // train Info
+    console.log("Fire: "+trainName);
+    console.log("Fire: "+trainDest);
+    console.log("Fire: "+trainFreqTime);
+    console.log("Fire: "+trainStart);
 
 
-    // Todo: check if input data makes sense
-    if (dataValidated) {
-        var newRow = $("<tr>");
-
-        // create row elements
-        var newName = $("<td>");
-        var newDestination = $("<td>");
-        var newStartTime = $("<td>");
-        var newFreq = $("<td>");
-        var newNextArrival = $("<td>");
-        var newTimeToArrival = $("<td>");
-
-        // add content from input form
-        newName.append($("#tName").val());
-        newDestination.append($("#tDestination").val());
-        newStartTime.append($("#tStartTime").val());
-        newFreq.append($("#tFreq").val());
-        newTimeToArrival.append("##");
-        newNextArrival.append("##:##");
+    // Current Time
+    var currentTime = moment().format("HH:mm");
+  
+    
+    var timeSinceStart = currentTime.dif(trainStart);
 
 
-        newRow.append(newName);
-        newRow.append(newDestination);
-        newRow.append(newStartTime);
-        newRow.append(newFreq);
-        newRow.append(newNextArrival);
-        newRow.append(newTimeToArrival);
-
-        // add row to dom
-        $("#trainSchedules").append(newRow);
-    }
-    else {
-
-    }
+    // TODO: find minutes to next train
+    // TODO: find time to next train
+  
+    
+    var minToNextTrain = trainFreqTime - (timeSinceStart % trainFreq);
+    console.log(minToNextTrain);
+  
+    // Calculate the total billed rate
+    var nextTrainTime = currentTime + minToNextTrain;
+    console.log(nextTrainTime);
+  
 
 
-
-
-
-
-
-})
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDest),
+      $("<td>").text(trainFreq),
+      $("<td>").text(minToNextTrain),
+      $("<td>").text(nextTrainTime),
+   
+    );
+  
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+  });
+  
+  // Example Time Math
+  // -----------------------------------------------------------------------------
+  // Assume Employee start date of January 1, 2015
+  // Assume current date is March 1, 2016
+  
+  // We know that this is 15 months.
+  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
+  
